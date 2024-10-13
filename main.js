c3287946833e70c454e7fa1be0fd4ae8b34d27e6
emilyt <emilyz_2001@hotmail.com>
2024-10-13 23:08:37 +0100

    swiper樣式練習2

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
 
-// swiper
+// swiper1
 var swiper = new Swiper(".mySwiper1", {
   loop: true,
   navigation: {
@@ -24,6 +24,31 @@ var swiper = new Swiper(".mySwiper1", {
       spaceBetween: 24,
     }
   }
+});
+
+//swiper2
+var swiper = new Swiper(".mySwiper2", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper2-button-next",
+    prevEl: ".swiper2-button-prev",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 24,
+      centeredSlides: true,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+      centeredSlides: true,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 
